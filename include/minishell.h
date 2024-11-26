b3d87cf75c4e42f5e449b7b68a28f86acfb2c7cd
Tommy Rischmann <trischma@made-f0Dr8s7.clusters.42paris.fr>
2024-11-26 14:42:32 +0100

    Push automatique :)

--- include/minishell.h
+++ include/minishell.h
@@ -1,2 +1,14 @@
-#include <readline/readline.h>
-#include <readline/history.h>
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+// typedef struct s_data
+// {
+
+// }				t_data;
+
+#endif
--- src/main.c
+++ src/main.c
@@ -9,9 +9,11 @@ void	initialize(int argc, char **argv, char **env)
 
 int	main(int argc, char **argv, char **env)
 {
+	// t_data data;
 	char	*command;
 
 	initialize(argc, argv, env);
+	// make env
 	while (1)
 	{
 		command = readline("minishell> ");

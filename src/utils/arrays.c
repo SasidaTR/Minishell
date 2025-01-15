f650e3155175c04557ce234a0b96aec2a90e0e32
Eve Arnera <earnera@paul-f3Br5s3.clusters.42paris.fr>
2025-01-15 11:36:37 +0100

    Norme

--- src/utils/arrays.c
+++ src/utils/arrays.c
@@ -1,26 +1,26 @@
 #include "../../include/minishell.h"
 
-void	ft_swap_array(char **a, char **b)
+void ft_swap_array(char **a, char **b)
 {
 	char	*temp;
-	
+
 	temp = *a;
 	*a = *b;
 	*b = temp;
 }
 
-void	sort_array(char **array, int n)
+void sort_array(char **array, int n)
 {
 	int	i;
 	int	j;
 
 	i = 0;
-	while(i < n)
+	while (i < n)
 	{
 		j = i + 1;
-		while(j < n)
+		while (j < n)
 		{
-			if(strncmp(array[i], array[j], INT_MAX) > 0)
+			if (strncmp(array[i], array[j], INT_MAX) > 0)
 				ft_swap_array(&array[i], &array[j]);
 			j++;
 		}

5c0a43d308369ea53f9c9b06ad29915c4fd8b8bf
Tommy Rischmann <trischma@bess-f2r5s6.clusters.42paris.fr>
2025-02-10 11:53:55 +0100

    Push automatique :)

--- src/main.c
+++ src/main.c
@@ -17,35 +17,6 @@ bool	empty_line(char *line)
 	return (false);
 }
 
-bool	parsing(t_data *data, char *line)
-{
-	if (open_quote(data, line))
-	{
-		free(line);
-		return (false);
-	}
-	if (!replace_dollar(&line, data) || !create_list_token(&data->token, line))
-	{
-		free(line);
-		free_all(data, ERR_MALLOC, EXT_MALLOC);
-	}
-	free(line);
-	if (data->token && data->token->prev->type == PIPE)
-	{
-		write(2, "Error: Unclosed pipe\n", 21);
-		data->exit_code = 2;
-		free_token(&data->token);
-		return (false);
-	}
-	if (!data->token || !create_list_cmd(data))
-	{
-		free_token(&data->token);
-		free_cmd(&data->commands);
-		return (false);
-	}
-	return (check_pipe(data));
-}
-
 int	make_env(t_data *data, char **env)
 {
 	t_list	*list;

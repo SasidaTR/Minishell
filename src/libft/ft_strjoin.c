3085efa4fc066210c9445d245956a536ab43fcee
Tommy Rischmann <trischma@bess-f3r2s2.clusters.42paris.fr>
2025-02-19 14:18:55 +0100

    Push automatique :)

--- src/libft/ft_strjoin.c
+++ src/libft/ft_strjoin.c
@@ -5,28 +5,18 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*str;
 	int		i;
 	int		j;
-	int		len1;
-	int		len2;
 
 	if (!s1 || !s2)
 		return (NULL);
-	len1 = ft_strlen(s1);
-	len2 = ft_strlen(s2);
-	str = malloc(len1 + len2 + 1);
+	str = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
 	if (!str)
 		return (NULL);
-	i = 0;
-	j = 0;
-	while (i < len1)
-	{
+	i = -1;
+	while (s1[++i])
 		str[i] = s1[i];
-		i++;
-	}
-	while (j < len2)
-	{
+	j = -1;
+	while (s2[++j])
 		str[i + j] = s2[j];
-		j++;
-	}
 	str[i + j] = '\0';
 	return (str);
 }
